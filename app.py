f02ad067c15404ff4d1eb510abc9ceb23cde88c2
Aditio <faditiokur@gmail.com>
2023-07-22 04:41:08 +0700

    Initialize project

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, flash, render_template, jsonify
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route('/')
+
+def home():
+   return render_template('index.html')
