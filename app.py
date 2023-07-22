c825db7e24149fa01b2644baaef4cc7d561116ea
Aditio <faditiokur@gmail.com>
2023-07-23 00:36:28 +0700

    Update preprocessing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, flash, render_template, jsonify
+from pathlib import Path
 from fetcher import get_ig_comments, get_tiktok_comments
 from preprocessing import sentiment, preprocessing_text, labeling_text
 import pandas as pd
@@ -45,57 +46,63 @@ def preprocessing():
 @app.route('/preprocessing-ajax', methods=['POST'])
 def preprocessing_ajax():
     if (request.form.get('preprocessing_ig')):
-        if (not os.path.exists('uploads/instagram_src.csv')):
-            return {'data': 'empty'}
+        instagram_clean_f = Path('uploads/instagram_clean.csv')
 
-        text = pd.read_csv('uploads/instagram_src.csv', encoding='latin-1')
-        json_text = text.to_json(orient='records')
-        json_text = json.loads(json_text)
+        if (instagram_clean_f.is_file()):
+            text = pd.read_csv('uploads/instagram_clean.csv', encoding='latin-1')
+            return text.to_json(orient='records')
+        else:
+            text = pd.read_csv('uploads/instagram_src.csv', encoding='latin-1')
+            json_text = text.to_json(orient='records')
+            json_text = json.loads(json_text)
 
-        comments = []
-        for comment in json_text:
-            username = comment['username']
-            comment_text = comment['comment']
-            clean_comment = preprocessing_text(comment_text)
-            labeled_comment = labeling_text(clean_comment)
-            sentiment_result = sentiment(clean_comment)
+            comments = []
+            for comment in json_text:
+                username = comment['username']
+                comment_text = comment['comment']
+                clean_comment = preprocessing_text(comment_text)
+                labeled_comment = labeling_text(clean_comment)
+                sentiment_result = sentiment(clean_comment)
 
-            comments.append({'username': username, 'comment': comment_text,
-                            'clean_comment': clean_comment, 'label': labeled_comment,
-                             'pos': sentiment_result['pos'], 'neg': sentiment_result['neg'], 'neu': sentiment_result['neu'],
-                             'compound': sentiment_result['compound']})
+                comments.append({'username': username, 'comment': comment_text,
+                                'clean_comment': clean_comment, 'label': labeled_comment,
+                                'pos': sentiment_result['pos'], 'neg': sentiment_result['neg'], 'neu': sentiment_result['neu'],
+                                'compound': sentiment_result['compound']})
 
-        pd.DataFrame(comments).to_csv(
-            'uploads/instagram_clean.csv',
-            header=['username', 'comment', 'clean_comment', 'label', 'pos', 'neg', 'neu', 'compund'],
-            index=False)
+            pd.DataFrame(comments).to_csv(
+                'uploads/instagram_clean.csv',
+                header=['username', 'comment', 'clean_comment', 'label', 'pos', 'neg', 'neu', 'compound'],
+                index=False)
 
-        return comments
+            return comments
 
     elif (request.form.get('preprocessing_tt')):
-        if (not os.path.exists('uploads/tiktok_src.csv')):
-            return {'data': 'empty'}
+        tiktok_clean_f = Path('uploads/tiktok_clean.csv')
 
-        text = pd.read_csv('uploads/tiktok_src.csv', encoding='latin-1')
-        json_text = text.to_json(orient='records')
-        json_text = json.loads(json_text)
+        if (tiktok_clean_f.is_file()):
+            text = pd.read_csv('uploads/tiktok_clean.csv', encoding='latin-1')
+            return text.to_json(orient='records')
+        else:
+            text = pd.read_csv('uploads/tiktok_src.csv', encoding='latin-1')
+            json_text = text.to_json(orient='records')
+            json_text = json.loads(json_text)
 
-        comments = []
-        for comment in json_text:
-            username = comment['username']
-            comment_text = comment['comment']
-            clean_comment = preprocessing_text(comment_text)
-            labeled_comment = labeling_text(clean_comment)
-            sentiment_result = sentiment(clean_comment)
+            comments = []
+            for comment in json_text:
+                username = comment['username']
+                comment_text = comment['comment']
+                clean_comment = preprocessing_text(comment_text)
+                labeled_comment = labeling_text(clean_comment)
+                sentiment_result = sentiment(clean_comment)
 
-            comments.append({'username': username, 'comment': comment_text,
-                            'clean_comment': clean_comment, 'label': labeled_comment,
-                             'pos': sentiment_result['pos'], 'neg': sentiment_result['neg'], 'neu': sentiment_result['neu'],
-                             'compound': sentiment_result['compound']})
+                comments.append({'username': username, 'comment': comment_text,
+                                'clean_comment': clean_comment, 'label': labeled_comment,
+                                'pos': sentiment_result['pos'], 'neg': sentiment_result['neg'], 'neu': sentiment_result['neu'],
+                                'compound': sentiment_result['compound']})
 
-        pd.DataFrame(comments).to_csv(
-            'uploads/tiktok_clean.csv',
-            header=['username', 'comment', 'clean_comment', 'label', 'pos', 'neg', 'neu', 'compund'],
-            index=False)
+            pd.DataFrame(comments).to_csv(
+                'uploads/tiktok_clean.csv',
+                header=['username', 'comment', 'clean_comment', 'label', 'pos', 'neg', 'neu', 'compound'],
+                index=False)
 
-        return comments
+            return comments
